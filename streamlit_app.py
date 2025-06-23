eaab30d5dd75a9241324719c748ab8942037f882
Ambicakesanakurthy <100029405+Ambicakesanakurthy@users.noreply.github.com>
2025-06-23 16:54:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,7 +99,7 @@ if st.button("Submit and Download") and tgml_file and excel_file and sheet_name:
                 inside_target_text = False
  
         # Save modified XML
-output_file = "updated_" + tgml_file.name
+        output_file = "updated_" + tgml_file.name
         tree.write(output_file, encoding="utf-8", xml_declaration=True)
  
         with open(output_file, "rb") as f:
