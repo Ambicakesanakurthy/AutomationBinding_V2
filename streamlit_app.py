719f466df4693bc90ecb1f9a84b0d72d9d043064
Ambicakesanakurthy <100029405+Ambicakesanakurthy@users.noreply.github.com>
2025-06-26 12:59:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.markdown("""
     }
  
     .form-box {
-        background-color: 	#0070AD;
+        background-color:#0070AD;
         padding: 40px 30px;
         border-radius: 15px;
         box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.2);
@@ -34,7 +34,7 @@ st.markdown("""
     .sub {
         text-align: center;
         font-size: 16px;
-        color: #ecf0f1;
+        color: #FF0090;
         margin-bottom: 30px;
     }
  
