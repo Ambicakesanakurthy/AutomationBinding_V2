95fe712fe60c55296b260fbff0d3d8df7752e103
Ambicakesanakurthy <100029405+Ambicakesanakurthy@users.noreply.github.com>
2025-07-01 11:32:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,17 +92,20 @@ if st.button("Submit and Download") and tgml_file and excel_file and sheet_name:
         for idx, row in df.iterrows():
             bind = str(row.get("Nomenclature", "")).strip()
             for col in ["First Label", "Second Label", "Third Label"]:
-                label = row.get(col)
-                if pd.isna(label) or not str(label).strip():
-                     continue
-                label = str(label).strip()
-                key = label.lower()
-                if key in seen_labels:
-                    st.error(f"Duplicate label found in Excel : '{label}' Row {idx+2}, column '{col}')")
-                    st.stop()
-                seen_labels.add(key)
-                label_to_bind[key] = bind
-                all_labels.append(key)
+                if "First Label" not in col:
+                     st.error(f"First Label in Excel sheet is not available, please check!")
+                else:
+                   label = row.get(col)
+                   if pd.isna(label) or not str(label).strip():
+                        continue
+                   label = str(label).strip()
+                   key = label.lower()
+                   if key in seen_labels:
+                       st.error(f"Duplicate label found in Excel : '{label}' Row {idx+2}, column '{col}')")
+                       st.stop()
+                   seen_labels.add(key)
+                   label_to_bind[key] = bind
+                   all_labels.append(key)
  
         # Replace in TGML
         in_group = False
