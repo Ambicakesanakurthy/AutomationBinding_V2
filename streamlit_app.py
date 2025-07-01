a4cedcbe6c8e0748e6008407652f502fdc31e4b5
Ambicakesanakurthy <100029405+Ambicakesanakurthy@users.noreply.github.com>
2025-07-01 12:11:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,28 +91,28 @@ if st.button("Submit and Download") and tgml_file and excel_file and sheet_name:
      
         for idx, row in df.iterrows():
             bind = str(row.get("Nomenclature", "")).strip()
-            for col in ["First Label", "Second Label", "Third Label"]:
-                if "First Label" not in col:
-                     st.error(f"First Label in Excel sheet is not available, please check!")
-                     break;
-                elif "Second Label" not in col:
-                     st.error(f"Second Label in Excel sheet is not available, please check!")
-                     break;
-                elif "Third Label" not in col:
-                     st.error(f"Third Label in Excel sheet is not available, please check!")
-                     break;
-                else:
-                   label = row.get(col)
-                   if pd.isna(label) or not str(label).strip():
-                        continue
-                   label = str(label).strip()
-                   key = label.lower()
-                   if key in seen_labels:
-                       st.error(f"Duplicate label found in Excel : '{label}' Row {idx+2}, column '{col}')")
-                       st.stop()
-                   seen_labels.add(key)
-                   label_to_bind[key] = bind
-                   all_labels.append(key)
+            if "First Label" not in col:
+                  st.error(f"First Label in Excel sheet is not available, please check!")
+                  break;
+            elif "Second Label" not in col:
+                  st.error(f"Second Label in Excel sheet is not available, please check!")
+                  break;
+            elif "Third Label" not in col:
+                  st.error(f"Third Label in Excel sheet is not available, please check!")
+                  break;
+            else:
+                  for col in ["First Label", "Second Label", "Third Label"]:
+                         label = row.get(col)
+                         if pd.isna(label) or not str(label).strip():
+                              continue
+                         label = str(label).strip()
+                         key = label.lower()
+                         if key in seen_labels:
+                             st.error(f"Duplicate label found in Excel : '{label}' Row {idx+2}, column '{col}')")
+                             st.stop()
+                         seen_labels.add(key)
+                         label_to_bind[key] = bind
+                         all_labels.append(key)
  
         # Replace in TGML
         in_group = False
