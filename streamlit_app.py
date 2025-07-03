f394ede29347e4f50c157648aeb76b8c7b059d16
Ambicakesanakurthy <100029405+Ambicakesanakurthy@users.noreply.github.com>
2025-07-03 12:58:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -154,7 +154,7 @@ if st.button("Submit and Download") and tgml_file and excel_file and sheet_name:
         output.seek(0)
  
         st.download_button("Download Updated TGML", output, file_name=f"updated_{tgml_file.name}", mime = "application/xml")
-        st.success("Binding completed successfully!/n{matched_label_count} bind names got replaced....")
+        st.success(f"Binding completed successfully!\n{matched_label_count} bind names got replaced....")
  
     except Exception as e:
         # shows error if something goes wrong
